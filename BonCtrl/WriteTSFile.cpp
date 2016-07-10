86adc9b674e15c3735b30cbee02e3387822cc7f6
c <>
2016-07-10 23:19:52 +0900

    1st

---
+++ BonCtrl/WriteTSFile.cpp
@@ -0,0 +1,401 @@
+#include "StdAfx.h"
+#include "WriteTSFile.h"
+#include <process.h>
+
+#include "../Common/PathUtil.h"
+#include "../Common/BlockLock.h"
+
+CWriteTSFile::CWriteTSFile(void)
+{
+	InitializeCriticalSection(&this->outThreadLock);
+
+    this->outThread = NULL;
+    this->outStopFlag = FALSE;
+	this->outStartFlag = FALSE;
+	this->overWriteFlag = FALSE;
+	this->createSize = 0;
+	this->subRecFlag = FALSE;
+	this->writeTotalSize = 0;
+	this->maxBuffCount = -1;
+}
+
+CWriteTSFile::~CWriteTSFile(void)
+{
+	EndSave();
+	DeleteCriticalSection(&this->outThreadLock);
+}
+
+//ファイル保存を開始する
+//戻り値：
+// TRUE（成功）、FALSE（失敗）
+//引数：
+// fileName				[IN]保存ファイル名
+// overWriteFlag		[IN]同一ファイル名存在時に上書きするかどうか（TRUE：する、FALSE：しない）
+// createSize			[IN]ファイル作成時にディスクに予約する容量
+// saveFolder			[IN]使用するフォルダ一覧
+// saveFolderSub		[IN]HDDの空きがなくなった場合に一時的に使用するフォルダ
+BOOL CWriteTSFile::StartSave(
+	wstring fileName,
+	BOOL overWriteFlag,
+	ULONGLONG createSize,
+	vector<REC_FILE_SET_INFO>* saveFolder,
+	vector<wstring>* saveFolderSub,
+	int maxBuffCount
+)
+{
+	if( saveFolder->size() == 0 ){
+		OutputDebugString(L"CWriteTSFile::StartSave Err saveFolder 0\r\n");
+		return FALSE;
+	}
+	
+	if( this->outThread == NULL ){
+		this->fileList.clear();
+		this->mainSaveFilePath = L"";
+		this->overWriteFlag = overWriteFlag;
+		this->createSize = createSize;
+		this->maxBuffCount = maxBuffCount;
+		this->writeTotalSize = 0;
+		this->subRecFlag = FALSE;
+		vector<REC_FILE_SET_INFO> saveFolder_ = *saveFolder;
+		this->saveFolderSub = *saveFolderSub;
+		for( size_t i=0; i<saveFolder_.size(); i++ ){
+			SAVE_INFO item;
+			item.writeUtil = NULL;
+			item.freeChk = FALSE;
+			item.writePlugIn = saveFolder_[i].writePlugIn;
+			if( item.writePlugIn.size() == 0 ){
+				item.writePlugIn = L"Write_Default.dll";
+			}
+			item.recFolder = saveFolder_[i].recFolder;
+			item.recFileName = saveFolder_[i].recFileName;
+			if( item.recFileName.size() == 0 ){
+				item.recFileName = fileName;
+			}
+			this->fileList.push_back(item);
+		}
+
+		//受信スレッド起動
+		this->outStopFlag = FALSE;
+		this->outStartFlag = FALSE;
+		this->outThread = (HANDLE)_beginthreadex(NULL, 0, OutThread, this, 0, NULL);
+		if( this->outThread ){
+			//保存開始まで待つ
+			while( WaitForSingleObject(this->outThread, 10) == WAIT_TIMEOUT && this->outStartFlag == FALSE );
+			if( this->outStartFlag ){
+				return TRUE;
+			}
+			CloseHandle(this->outThread);
+			this->outThread = NULL;
+		}
+	}
+
+	OutputDebugString(L"CWriteTSFile::StartSave Err 1\r\n");
+	return FALSE;
+}
+
+//保存サブフォルダから空きのあるフォルダパスを取得
+//戻り値：
+// TRUE（成功）、FALSE（失敗）
+//引数：
+// needFreeSize			[IN]最低必要な空きサイズ
+// freeFolderPath		[OUT]見つかったフォルダ
+BOOL CWriteTSFile::GetFreeFolder(
+	ULONGLONG needFreeSize,
+	wstring& freeFolderPath
+)
+{
+	BOOL ret = FALSE;
+
+	for( int i = 0; i < (int)this->saveFolderSub.size(); i++ ){
+		ULARGE_INTEGER stFree;
+		if( _GetDiskFreeSpaceEx( this->saveFolderSub[i].c_str(), &stFree, NULL, NULL ) != FALSE ){
+			if( stFree.QuadPart > needFreeSize ){
+				freeFolderPath = this->saveFolderSub[i];
+				ChkFolderPath(freeFolderPath);
+				ret = TRUE;
+				break;
+			}
+		}
+	}
+	return ret;
+}
+
+//指定フォルダの空きがあるかチェック
+//戻り値：
+// TRUE（成功）、FALSE（失敗）
+//引数：
+// needFreeSize			[IN]最低必要な空きサイズ
+// chkFolderPath		[IN]指定フォルダ
+BOOL CWriteTSFile::ChkFreeFolder(
+	ULONGLONG needFreeSize,
+	wstring chkFolderPath
+)
+{
+	BOOL ret = FALSE;
+
+	ULARGE_INTEGER stFree;
+	if( _GetDiskFreeSpaceEx( chkFolderPath.c_str(), &stFree, NULL, NULL ) != FALSE ){
+		if( stFree.QuadPart > needFreeSize ){
+			ret = TRUE;
+		}
+	}
+	return ret;
+}
+
+//ファイル保存を終了する
+//戻り値：
+// TRUE（成功）、FALSE（失敗）
+BOOL CWriteTSFile::EndSave()
+{
+	BOOL ret = TRUE;
+
+	if( this->outThread != NULL ){
+		this->outStopFlag = TRUE;
+		// スレッド終了待ち
+		if ( ::WaitForSingleObject(this->outThread, 15000) == WAIT_TIMEOUT ){
+			::TerminateThread(this->outThread, 0xffffffff);
+		}
+		CloseHandle(this->outThread);
+		this->outThread = NULL;
+	}
+
+	this->tsBuffList.clear();
+
+	return ret;
+}
+
+//出力用TSデータを送る
+//戻り値：
+// TRUE（成功）、FALSE（失敗）
+//引数：
+// data		[IN]TSデータ
+// size		[IN]dataのサイズ
+BOOL CWriteTSFile::AddTSBuff(
+	BYTE* data,
+	DWORD size
+	)
+{
+	if( data == NULL || size == 0 || this->outThread == NULL){
+		return FALSE;
+	}
+
+	BOOL ret = TRUE;
+
+	{
+		CBlockLock lock(&this->outThreadLock);
+		for( std::list<vector<BYTE>>::iterator itr = this->tsBuffList.begin(); size != 0; itr++ ){
+			if( itr == this->tsBuffList.end() ){
+				//バッファを増やす
+				if( this->maxBuffCount > 0 && this->tsBuffList.size() > (size_t)this->maxBuffCount ){
+					_OutputDebugString(L"★writeBuffList MaxOver");
+					for( itr = this->tsBuffList.begin(); itr != this->tsBuffList.end(); (itr++)->clear() );
+					itr = this->tsBuffList.begin();
+				}else{
+					this->tsBuffList.push_back(vector<BYTE>());
+					itr = this->tsBuffList.end();
+					(--itr)->reserve(48128);
+				}
+			}
+			DWORD insertSize = min(48128 - (DWORD)itr->size(), size);
+			itr->insert(itr->end(), data, data + insertSize);
+			data += insertSize;
+			size -= insertSize;
+		}
+	}
+	return ret;
+}
+
+UINT WINAPI CWriteTSFile::OutThread(LPVOID param)
+{
+	//プラグインがCOMを利用するかもしれないため
+	CoInitialize(NULL);
+
+	CWriteTSFile* sys = (CWriteTSFile*)param;
+	BOOL emptyFlag = TRUE;
+	for( size_t i=0; i<sys->fileList.size(); i++ ){
+		sys->fileList[i].writeUtil = new CWritePlugInUtil;
+		wstring moduleFolder;
+		GetModuleFolderPath(moduleFolder);
+		if( sys->fileList[i].writeUtil->Initialize((moduleFolder + L"\\Write\\" + sys->fileList[i].writePlugIn).c_str()) == FALSE ){
+			OutputDebugString(L"CWriteTSFile::StartSave Err 3\r\n");
+			SAFE_DELETE(sys->fileList[i].writeUtil);
+		}else{
+			wstring folderPath = sys->fileList[i].recFolder;
+			ChkFolderPath(folderPath);
+			if( CompareNoCase(sys->fileList[i].writePlugIn, L"Write_Default.dll") == 0 ){
+				//デフォルトの場合は空き容量をあらかじめチェック
+				if( sys->createSize > 0 ){
+					if( sys->ChkFreeFolder(sys->createSize, sys->fileList[i].recFolder) == FALSE ){
+						if( sys->GetFreeFolder(sys->createSize, folderPath) ){
+							//空きなかったのでサブフォルダに録画
+							sys->subRecFlag = TRUE;
+						}
+					}
+				}
+			}
+			//開始
+			BOOL startRes = sys->fileList[i].writeUtil->StartSave(
+				(folderPath + L'\\' + sys->fileList[i].recFileName).c_str(), sys->overWriteFlag, sys->createSize);
+			if( startRes == FALSE ){
+				OutputDebugString(L"CWriteTSFile::StartSave Err 2\r\n");
+				//エラー時サブフォルダでリトライ
+				if( sys->GetFreeFolder(sys->createSize, folderPath) ){
+					//空きなかったのでサブフォルダに録画
+					sys->subRecFlag = TRUE;
+					startRes = sys->fileList[i].writeUtil->StartSave(
+						(folderPath + L'\\' + sys->fileList[i].recFileName).c_str(), sys->overWriteFlag, sys->createSize);
+				}
+			}
+			if( startRes == FALSE ){
+				SAFE_DELETE(sys->fileList[i].writeUtil);
+			}else{
+				if( i == 0 ){
+					WCHAR saveFilePath[512] = L"";
+					DWORD saveFilePathSize = 512;
+					sys->fileList[i].writeUtil->GetSaveFilePath(saveFilePath, &saveFilePathSize);
+					sys->mainSaveFilePath = saveFilePath;
+				}
+				sys->fileList[i].freeChk = emptyFlag;
+				emptyFlag = FALSE;
+			}
+		}
+	}
+	if( emptyFlag ){
+		OutputDebugString(L"CWriteTSFile::StartSave Err fileList 0\r\n");
+		CoUninitialize();
+		return 0;
+	}
+	sys->outStartFlag = TRUE;
+	std::list<vector<BYTE>> data;
+
+	while( sys->outStopFlag == FALSE ){
+		//バッファからデータ取り出し
+		{
+			CBlockLock lock(&sys->outThreadLock);
+			if( data.empty() == false ){
+				//返却
+				data.front().clear();
+				std::list<vector<BYTE>>::iterator itr;
+				for( itr = sys->tsBuffList.begin(); itr != sys->tsBuffList.end() && itr->empty() == false; itr++ );
+				sys->tsBuffList.splice(itr, data);
+			}
+			if( sys->tsBuffList.empty() == false && sys->tsBuffList.front().size() == 48128 ){
+				data.splice(data.end(), sys->tsBuffList, sys->tsBuffList.begin());
+			}
+		}
+
+		if( data.empty() == false ){
+			DWORD dataSize = (DWORD)data.front().size();
+			for( size_t i=0; i<sys->fileList.size(); i++ ){
+				{
+					if( sys->fileList[i].writeUtil != NULL ){
+						DWORD write = 0;
+						if( sys->fileList[i].writeUtil->AddTSBuff( &data.front().front(), dataSize, &write) == FALSE ){
+							//空きがなくなった
+							if( i == 0 ){
+								CBlockLock lock(&sys->outThreadLock);
+								if( sys->writeTotalSize >= 0 ){
+									//出力サイズの加算を停止する
+									sys->writeTotalSize = -(sys->writeTotalSize + 1);
+								}
+							}
+							sys->fileList[i].writeUtil->StopSave();
+
+							if( sys->fileList[i].freeChk == TRUE ){
+								//次の空きを探す
+								wstring freeFolderPath = L"";
+								if( sys->GetFreeFolder(200*1024*1024, freeFolderPath) == TRUE ){
+									wstring recFilePath = freeFolderPath;
+									recFilePath += L"\\";
+									recFilePath += sys->fileList[i].recFileName;
+
+									//開始
+									if( sys->fileList[i].writeUtil->StartSave(recFilePath.c_str(), sys->overWriteFlag, 0) == FALSE ){
+										//失敗したので終わり
+										SAFE_DELETE(sys->fileList[i].writeUtil);
+									}else{
+										sys->subRecFlag = TRUE;
+
+										if( dataSize > write ){
+											sys->fileList[i].writeUtil->AddTSBuff( &data.front().front()+write, dataSize-write, &write);
+										}
+									}
+								}
+							}else{
+								//失敗したので終わり
+								SAFE_DELETE(sys->fileList[i].writeUtil);
+							}
+						}else{
+							//原作では成否にかかわらずwriteTotalSizeにdataSizeを加算しているが
+							//出力サイズの利用ケース的にはmainSaveFilePathと一致させないとおかしいと思うので、そのように変更した
+							if( i == 0 ){
+								CBlockLock lock(&sys->outThreadLock);
+								if( sys->writeTotalSize >= 0 ){
+									sys->writeTotalSize += dataSize;
+								}
+							}
+						}
+					}
+				}
+			}
+		}else{
+			//TODO: 厳密にはメッセージをディスパッチすべき(スレッド内で単純なCOMオブジェクトを扱う限りは(多分)問題ない)
+			Sleep(100);
+		}
+	}
+
+	//残っているバッファを書き出し
+	{
+		CBlockLock lock(&sys->outThreadLock);
+		for( std::list<vector<BYTE>>::iterator itr = sys->tsBuffList.begin(); itr != sys->tsBuffList.end() && itr->empty() == false; itr++ ){
+			for( size_t i=0; i<sys->fileList.size(); i++ ){
+				if( sys->fileList[i].writeUtil ){
+					DWORD write = 0;
+          //sys->fileList[i].writeUtil->AddTSBuff(&itr->front(), (DWORD)itr->size(), &write
+          /* WriteDllTester_edcb */
+          if (sys->fileList[i].writeUtil->AddTSBuff(&itr->front(), (DWORD)itr->size(), &write)){
+            sys->writeTotalSize += write;
+          }
+
+
+				}
+			}
+			itr->clear();
+		}
+	}
+	for( size_t i=0; i<sys->fileList.size(); i++ ){
+		if( sys->fileList[i].writeUtil ){
+			sys->fileList[i].writeUtil->StopSave();
+			SAFE_DELETE(sys->fileList[i].writeUtil);
+		}
+	}
+
+	CoUninitialize();
+	return 0;
+}
+
+//録画中のファイルのファイルパスを取得する
+//引数：
+// filePath			[OUT]保存ファイル名
+// subRecFlag		[OUT]サブ録画が発生したかどうか
+void CWriteTSFile::GetSaveFilePath(
+	wstring* filePath,
+	BOOL* subRecFlag
+	)
+{
+	*filePath = this->mainSaveFilePath;
+	*subRecFlag = this->subRecFlag;
+}
+
+//録画中のファイルの出力サイズを取得する
+//引数：
+// writeSize			[OUT]保存ファイル名
+void CWriteTSFile::GetRecWriteSize(
+	__int64* writeSize
+	)
+{
+	if( writeSize != NULL ){
+		CBlockLock lock(&this->outThreadLock);
+		*writeSize = this->writeTotalSize < 0 ? -(this->writeTotalSize + 1) : this->writeTotalSize;
+	}
+}
---
+++ WriteDllTester_edcb/WriteDllTester_edcb/WriteDllTester_edcb.cpp
@@ -0,0 +1,328 @@
+/*
+
+based on [ xtne6f/EDCB work-plus-s 160616 ]
+
+
+command line               [1]           [2]            [3]               [4]           [5]      [6]
+  WriteDllTester_edcb.exe  "input_path"  "save_folder"  "save_foldersub"  limit_MiBsec  dllname  overwrite
+
+
+　args[1]              [2]              [3]                 [4]           [5]                [6]
+  "input_file"         "output_folder"  "output_foldersub"  limit_MiBsec  dll_name           overwrite
+  "D:\input.ts"        "D:\\"           "E:\\"              0             Write_Default.dll  1
+  "D:\input.ts"        "D:\rec\\"       "E:\\"              2.0
+  "E:\TS_Samp\t2s.ts"  "E:\pf_out"      "D:"                20.0          Write_PF.dll       1
+
+
+  ・フォルダ名最後の\は\\にする。\だけだと特殊文字\"になる。
+  ・引数間に全角スペースがあるとダメ
+*/
+
+
+
+// WriteDllTester_edcb.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include "Shlwapi.h"
+#pragma comment(lib, "shlwapi.lib")
+
+using namespace std;
+using namespace std::chrono;
+
+#include "../../BonCtrl/WriteTSFile.h"
+
+
+
+
+//==================================
+// func
+//==================================
+bool CheckDLL(wstring dllname);
+void ReadSpeed_CrackDown(double limit, __int64 read);
+
+
+//==================================
+// main
+//==================================
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
+{
+  ///*  test args  */
+  //wchar_t *test_argv[16] = {};
+  //test_argv[0] = L"app_path";
+  //test_argv[1] = L"E:\\TS_Samp\\t2s.ts";     // input path
+  //test_argv[2] = L"D:\\";                    // save_folder
+  //test_argv[3] = L"none foldersub";          // save_foldersub
+  //test_argv[4] = L"-1";                      // limit
+  //test_argv[5] = L"Write_Default.dll";       // dllname
+  //test_argv[6] = L"1";                       // overwrite
+  //argc = 7;
+  //argv = test_argv;
+
+
+  //setting
+  wstring input_file, save_folder, save_foldersub;
+  double limit_MiB = 0;
+  wstring dllname;
+  bool overwrite = true;
+
+
+  /*  test setting  */
+  //input_file = L"E:\\TS_Samp\\t60s.ts";
+  //input_file = L"E:\\TS_Samp\\t2s.ts";
+  //save_folder = L"";
+  //save_folder = L"D:\\";
+  //save_foldersub = L"E:\\";
+  //limit_MiB = 1.7;
+  //limit_MiB = 20.0;
+  //limit_MiB = 0;
+  //dllname = L"Write_PF.dll";
+  //dllname = L"Write_Default.dll";
+  //overwrite = true;
+
+
+  //setting from commandline
+  if (2 <= argc) input_file = argv[1];
+  if (3 <= argc) save_folder = argv[2];
+  if (4 <= argc) save_foldersub = argv[3];
+  if (5 <= argc)
+  {
+    if ((_snwscanf_s(argv[4], 6, L"%lf", &limit_MiB)) <= 0)
+      limit_MiB = 0;
+  }
+  if (6 <= argc) dllname = argv[5];
+  if (7 <= argc)
+  {
+    /*
+      overwrite = argv[6] == L"1";だと正しく処理できない。
+      wchar_t *  -->  wstring
+    */
+    overwrite = wstring(argv[6]) == L"1";
+  }
+
+  //show setting
+  wcerr << endl;
+  wcerr << L"  input_file     : " << input_file << endl;
+  wcerr << L"  save_folder    : " << save_folder << endl;
+  wcerr << L"  save_foldersub : " << save_foldersub << endl;
+  wcerr << fixed << setprecision(1);       //小数点１桁まで表示
+  wcerr << L"  limit MiB/sec  : " << limit_MiB << endl;
+  wcerr << L"  dllname        : " << dllname << endl;
+  wcerr << L"  overwrite      : " << overwrite << endl;
+  wcerr << L"--------------------------------------------" << endl;
+  wcerr << endl;
+
+
+
+
+
+  //初期化
+  //dll
+  bool checkOK = CheckDLL(dllname);
+  if (checkOK == false)
+  {
+    Sleep(2000);
+    return 1;
+  }
+
+  //input
+  ifstream ifs(input_file, std::ios_base::in | std::ios_base::binary);
+  if (!ifs) {
+    wcerr << endl;
+    wcerr << L"fail to open file" << endl;
+    Sleep(2000);
+    return 1;
+  }
+
+  //output
+  CWriteTSFile writeTSFile;
+  {
+    wstring filename = PathFindFileName(input_file.c_str());
+
+    vector<REC_FILE_SET_INFO> saveFolder;
+    REC_FILE_SET_INFO folderItem;
+    {
+      folderItem.recFolder = save_folder;
+      folderItem.writePlugIn = dllname;
+      folderItem.recNamePlugIn = L"";
+      folderItem.recFileName = L"";
+    }
+    saveFolder.emplace_back(folderItem);
+
+    vector<wstring> saveFolderSub;
+    {
+      saveFolderSub.emplace_back(save_foldersub);
+    }
+
+    //start save
+    const ULONGLONG WriteBuffSize = 770448;
+    BOOL canSave = writeTSFile.StartSave(filename, overwrite, WriteBuffSize, &saveFolder, &saveFolderSub, 770048);
+    if (canSave == FALSE)
+    {
+      Sleep(2000);
+      return 1;
+    }
+  }
+
+
+
+  //
+  // main loop
+  //
+  const int chunk = 188 * 5;
+  double limit = limit_MiB * 1024 * 1024;
+
+  __int64 TotalRead = 0;
+  __int64 ErrorCounter = 0;
+  while (true)
+  {
+    //進捗
+    if (TotalRead % (chunk * 50 * 1024) == 0)  // tick tock 50 MB
+    {
+      int read_MiB = (int)(1.0 * TotalRead / 1024 / 1024);
+      //wcerr << L"  tester read  " << std::setw(4) << read_MiB << "  MiB" << endl;
+    }
+
+    //読
+    char data[chunk] = {};
+    DWORD read = (DWORD)ifs.read((char *)data, sizeof(char) *chunk).gcount();
+    TotalRead += read;
+    if (read == 0 && ifs.eof())
+      break;
+    //速度制限
+    ReadSpeed_CrackDown(limit, read);
+
+
+    //書
+    BOOL success = writeTSFile.AddTSBuff((BYTE *)&data, read);
+    if (success == FALSE)
+    {
+      ErrorCounter++;
+      __int64 writeSize = 0;
+      writeTSFile.GetRecWriteSize(&writeSize);
+      wcerr << endl;
+      wcerr << L"AddTSBuff error :" << endl;
+      wcerr << L"    TotalRead  = " << TotalRead << endl;
+      wcerr << L"    TotalWrite = " << writeSize << endl;
+    }
+
+  }//while
+  writeTSFile.EndSave();
+
+
+
+  //show result
+  __int64 writeSize = 0;
+  writeTSFile.GetRecWriteSize(&writeSize);
+  wcerr << endl;
+  wcerr << L"--------------------------------------------" << endl;
+  wcerr << L"tester" << endl;
+  wcerr << L"  TotalRead     =  " << TotalRead << endl;
+  wcerr << L"  TotalWrite    =  " << writeSize << endl;
+  wcerr << L"  ErrorCounter  =  " << ErrorCounter << endl;
+  wcerr << endl;
+  wcerr << endl;
+  wcerr << L"  finish" << endl;
+  Sleep(4 * 1000);
+
+  return 0;
+}
+
+
+
+
+
+
+//==================================
+// CheckDLL
+//==================================
+bool CheckDLL(wstring dllname)
+{
+  wstring writeFolder;
+  {
+    wchar_t exePath[_MAX_PATH];
+    GetModuleFileName(NULL, exePath, sizeof(exePath));
+    wchar_t drive[_MAX_DRIVE], dir[_MAX_DIR];
+    _tsplitpath_s(exePath, drive, _MAX_DRIVE, dir, _MAX_DIR, NULL, NULL, NULL, NULL);
+    writeFolder = wstring(drive) + wstring(dir) + L"Write\\";
+  }
+  CreateDirectory(writeFolder.c_str(), NULL);
+
+
+  wstring dllpath = writeFolder + dllname;
+  ifstream ifs(dllpath, std::ios_base::in | std::ios_base::binary);
+  if (!ifs) {
+    wcerr << endl;
+    wcerr << L"dll not found" << endl;
+    return false;
+  }
+
+
+  CWritePlugInUtil util;
+  BOOL success = util.Initialize(dllpath.c_str());
+  if (success)
+  {
+    return true;
+  }
+  else
+  {
+    wcerr << endl;
+    wcerr << L"fail to initialize dll" << endl;
+    return false;
+  }
+}
+
+
+//==================================
+// 速度制限
+//==================================
+auto tickBeginTime = system_clock::now();        //速度制限    計測開始時間
+__int64 tickReadSize = 0;                        //速度制限    200ms間の読込み量
+void ReadSpeed_CrackDown(double limit, __int64 read)
+{
+  if (limit <= 0) return;
+
+  tickReadSize += read;
+
+  auto tickElapse = system_clock::now() - tickBeginTime;
+  auto elapse_ms = duration_cast<chrono::milliseconds>(tickElapse).count();
+
+  //200msごとにカウンタリセット
+  if (200 <= elapse_ms)
+  {
+    tickBeginTime = system_clock::now();
+    tickReadSize = 0;
+  }
+
+  //読込量が制限をこえたらsleep_for
+  if (limit * (200.0 / 1000.0) < tickReadSize)
+    this_thread::sleep_for(chrono::milliseconds(200 - elapse_ms));
+
+}
+
+
+
+//==================================
+// OutputDebugStringをコンソールに表示
+//==================================
+void OutputDebugStringWrapper(LPCWSTR lpOutputString)
+{
+  wcerr << lpOutputString << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
